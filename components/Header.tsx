a92367870a62c2a5ecd3a3ff1ee45e017887673e
Austin <astoller66@gmail.com>
2024-05-04 18:42:24 -0500

    server work

---
+++ components/Header.tsx
@@ -0,0 +1,33 @@
+import { MagnifyingGlassIcon } from "@heroicons/react/16/solid";
+
+
+
+function Header() {
+  return (
+    <header>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <img
+          src="https://rb.gy/ulxxee"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
+      </div>
+
+      <div>
+        <MagnifyingGlassIcon className="hidden h-6 w-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
