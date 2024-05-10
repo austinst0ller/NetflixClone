bf84a1ef89dd6e62a9aae056df6aaeda77190d18
Austin <astoller66@gmail.com>
2024-05-10 07:51:12 -0500

    new

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import requests from '@/utils/requests';
 import Head from "next/head";
 
 const Home = ({ netflixOriginls }) => {
+  console.log(netflixOriginls)
   return (
     <div className='relative h-screen bg-gradient-to-b from-gray-900/10 
     to-[#010511] lg:h-[140vh]'>
