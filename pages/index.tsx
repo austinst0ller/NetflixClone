b60bcc6ce03b603624fd43f69b2e9aa4c5e9563e
Austin <astoller66@gmail.com>
2024-05-09 23:10:05 -0500

    netflix clone

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
+import Banner from '@/components/Banner';
 import Header from '@/components/Header';
 import type { NextPage } from 'next'
 import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center py-2'>
+    <div className='relative h-screen bg-gradient-to-b from-gray-900/10 
+    to-[#010511] lg:h-[140vh]'>
       <Head>
         <title>Home - Netflix</title>
       </Head>
@@ -13,6 +15,7 @@ const Home: NextPage = () => {
 
       <main>
         {/* Banner */}
+        <Banner />
         <section>
           {/* Row */}
           {/* Row */}
